5262b72f4145617b6f71a1802e2cb738135226fd
MosiBot <mosiambot@gmail.com>
2016-09-24 10:18:53 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -45,5 +45,5 @@ else
     exit 1
   fi
   rm -r ../.telegram-cli/state #Prevent tg from crash
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/Roonx.lua -l 1 -E $@
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./RoonxBot/Roonx.lua -l 1 -E $@
 fi
